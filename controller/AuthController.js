d1631bea5cd148608d6114edf3169c60a302f30f
Guilherme Sella <guilhermesella1974@gmail.com>
2023-10-18 23:30:05 -0300

    validando criptografia da senha na autenticação

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,8 +1,9 @@
 const User = require("../model/User")
 const express = require("express")
+const bcrypt = require("bcryptjs")
 const router = express.Router();
 
-
+// Rota de registro com senha criptografada
 router.post("/registrar", async(req,res)=>{
 
     const {email} = req.body;
@@ -20,5 +21,32 @@ router.post("/registrar", async(req,res)=>{
         data: user
     })
 })
+
+
+// Rota de autenticação
+router.post("/authenticate", async(req,res)=>{
+    const {email, senha} = req.body;
+
+    const user = await User.findOne({email}).select("+senha");
+
+    if(!user) {
+        return res.status(400).json({
+            message:"Usuário não encontrado!"
+        })
+    }
+
+    if(!await bcrypt.compare(senha, user.senha)){
+        return res.status(400).send({
+            message:"Senha inválida"
+        })
+    }
+
+    user.senha = undefined;
+
+    return res.json(user)
+
+})
+
+
 
 module.exports = router;
