d45f6f577125f04f862e7f2b863a4fd7769016de
Guilherme Sella <guilhermesella1974@gmail.com>
2023-10-18 23:10:32 -0300

    Registrando user com senha criptografada e verificando se o mesmo já existe

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -3,19 +3,21 @@ const express = require("express")
 const router = express.Router();
 
 
-router.post("/registrar", (req,res)=>{
+router.post("/registrar", async(req,res)=>{
 
     const {email} = req.body;
 
-    if(User.findOne({email})){
-        return res.json({
+    if(await User.findOne({email})){
+        return res.status(400).json({
             message:"Usuário já existe"
         })
     }
 
+    const user = await User.create(req.body);
 
     return res.json({
-        message:"Registrar"
+        message:"Usuário criado com sucesso!",
+        data: user
     })
 })
 
--- model/User.js
+++ model/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("../db/db")
+const bcryptjs = require("bcryptjs")
+
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -21,6 +23,15 @@ const UserSchema = new mongoose.Schema({
         default:Date.now
     }
 
+})
+
+// Bloco que será executado antes do usuário ser criado
+
+UserSchema.pre("save", async function(next){
+    //Criptografando senha 10 vezes
+    const hash = await bcryptjs.hash(this.senha, 10);
+    console.log(hash);
+    this.senha = hash;
 })
 
 const User = mongoose.model("User", UserSchema);
